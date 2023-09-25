ede5d0c8bf7c96fe33cb2a7b26fe9c8670cc808b
keisuke-a <36350477+keisuke-a@users.noreply.github.com>
2023-09-25 17:12:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
-import streamlit
-import openai
-import langchain
-import duckduckgo-search
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-
-st_callback = StreamlitCallbackHandler(st.container())
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import AgentType, initialize_agent, Tool
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain import LLMMathChain
 
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
-llm = ChatOpenAI(temperature=0, streaming=True)
-tools = load_tools(["ddg-search"]) # DuckDuckGoの検索ツールをロードする
+st_callback = StreamlitCallbackHandler(st.container())
+search = DuckDuckGoSearchRun()
+llm = ChatOpenAI(temperature=0, streaming=True, model="gpt-3.5-turbo")
+llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
 
-# ReActエージェントを初期化する。このエージェントはzero-shot学習と説明を組み合わせたもの
+# 使用可能なツールのリストを作成する
+tools = [
+    Tool(
+        name = "ddg-search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events. You should ask targeted questions"
+    ),
+    Tool(
+        name="Calculator",
+        func=llm_math_chain.run,
+        description="useful for when you need to answer questions about math"
+    ),
+]
+
+# エージェントを初期化する。このエージェントはOpenAIの関数を使用
 agent = initialize_agent(
-    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True
 )
 
-if prompt := st.chat_input(): # Streamlit のチャット入力がある場合に実行する
-    st.chat_message("user").write(prompt) # ユーザの入力メッセージをStreamlitのチャットに表示する
-    with st.chat_message("assistant"): # アシスタントの応答を表示するためのブロックを開始する
-        st_callback = StreamlitCallbackHandler(st.container()) # Streamlitのコンテナをコールバックとして使用するハンドラを初期化する****
-        response = agent.run(prompt, callbacks=[st_callback]) # エージェントを使って
-        st.write(response) # 応答をStreamlitのチャットに表示する
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
