c3cb4c69507ffc7f20f0ca832282f62d424ff467
keisuke-a <36350477+keisuke-a@users.noreply.github.com>
2023-09-25 16:40:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,20 @@
-# 以下を「app.py」に書き込み
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-import openai
-import langchain
-import os
-
-#langchain関連のモジュールを読み込み
-from langchain.llms import OpenAI
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-from langchain.memory import ConversationBufferMemory
 
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
-os.environ["OPENAI_API_KEY"] = st.secrets.OpenAIAPI.openai_api_key
+llm = ChatOpenAI(temperature=0, streaming=True)
+tools = load_tools(["ddg-search"]) # DuckDuckGoの検索ツールをロードする
 
-llm = OpenAI(model_name="text-davinci-003", temperature=0.2)
-tool_names = ["llm-math"]
-tools = load_tools(tool_names, llm=llm)
-
-memory = ConversationBufferMemory(
-    memory_key="chat_history",
-    return_messages=True,
-)
-
+# ReActエージェントを初期化する。このエージェントはzero-shot学習と説明を組み合わせたもの
 agent = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-    memory=memory,
-    verbose=True
+    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
-system_prompt = """
-"""
-
-# st.session_stateを使いメッセージのやりとりを保存
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": system_prompt}
-        ]
-
-# チャットボットとやりとりする関数
-def communicate():
-    messages = st.session_state["messages"]
-    bot_message = agent.run(st.session_state["user_input"])
-    messages.append(st.session_state["user_input"])
-    messages.append(bot_message)
-    st.session_state["user_input"] = ""  # 入力欄を消去
-
-
-# ユーザーインターフェイスの構築
-st.title("計算.bot")
-st.write("計算問題を出してください。")
-
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
-
-if st.session_state["messages"]:
-    messages = st.session_state["messages"]
-
-    for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        st.write(message)
+if prompt := st.chat_input(): # Streamlit のチャット入力がある場合に実行する
+    st.chat_message("user").write(prompt) # ユーザの入力メッセージをStreamlitのチャットに表示する
+    with st.chat_message("assistant"): # アシスタントの応答を表示するためのブロックを開始する
+        st_callback = StreamlitCallbackHandler(st.container()) # Streamlitのコンテナをコールバックとして使用するハンドラを初期化する****
+        response = agent.run(prompt, callbacks=[st_callback]) # エージェントを使って
+        st.write(response) # 応答をStreamlitのチャットに表示する
